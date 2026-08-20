366cae63b9bd650d4d9c3607d9ad6243ce70e7cd
Eduardo <eduardo852823@gmail.com>
2026-08-20 17:38:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ function toggleFullscreen() {
         document.documentElement.requestFullscreen();
         btn.classList.add('hidden');
     } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        }
+        if (document.exitFullscreen) document.exitFullscreen();
     }
 }
-
 document.addEventListener("fullscreenchange", () => {
     const btn = document.querySelector('.btn-fs');
-    if (!document.fullscreenElement) {
-        btn.classList.remove('hidden');
-    }
+    if (!document.fullscreenElement) btn.classList.remove('hidden');
 });
+
+function checkAnswer(isCorrect) {
+    const feedback = document.getElementById('feedback');
+    if (isCorrect) {
+        feedback.innerText = "Correto! A Guerra Fria impulsionou ditaduras e regimes militares em diversos países latino-americanos alinhados aos blocos.";
+        feedback.style.color = "#48bb78";
+    } else {
+        feedback.innerText = "Incorreto. Tente novamente!";
+        feedback.style.color = "#f56565";
+    }
+}
